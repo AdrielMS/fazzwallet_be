f9da11811a13021be2b4d925e5f80bab09c90919
AdrielMS <adrielsjahranie@gmail.com>
2023-02-15 01:31:55 +0700

    modified:   helper(db)/formUpload.js 	new file:   src/controller/controller_transaction.js 	new file:   src/model/model_transaction.js 	modified:   src/route/route_auth.js 	modified:   src/route/route_index.js 	new file:   src/route/route_transaction.js

--- src/route/route_auth.js
+++ src/route/route_auth.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express();
+const multer = require("multer");
+const formUpload = require("../../helper(db)/formUpload");
 
 const authController = require("../controller/controller_auth");
 
 router.post("/login", authController.login);
 router.post("/register", authController.register);
-router.get("/users", authController.get);
-router.get("/users/:id", authController.getDetail);
-router.patch("/users/:id", authController.update);
+router.get("/users", formUpload.single("img"), authController.get);
+router.get("/users/:id", formUpload.single("img"), authController.getDetail);
+router.patch("/users/:id", formUpload.single("img"), authController.update);
 
 module.exports = router;
--- src/route/route_index.js
+++ src/route/route_index.js
@@ -4,15 +4,17 @@ const router = express();
 //import route mvp
 const authRoute = require("../route/route_auth");
 const historyRoute = require("../route/route_history");
+const transactionRoute = require("../route/route_transaction");
 // end
 
 router.get("/", (req, res) => {
-  return res.send("Backend for Hiringme");
+  return res.send("Backend for Fazzwallet");
 });
 
 // route.use("/apa", panggil inisial importnya)
 router.use("/auth", authRoute);
 router.use("/profile", historyRoute);
+router.use("/profile", transactionRoute);
 // router.use("/Profile", historyRoute);
 //end
 
